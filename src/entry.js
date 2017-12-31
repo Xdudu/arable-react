709edf96302299478d052b06123bccc4404b31e9
Xdudu <liuyuewuyu@yeah.net>
2017-12-31 16:00:17 +0800

    Init

---
+++ src/entry.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+import Seed from './seed.js'
+
+import './index.css'
+
+
+render(<Seed />, document.getElementById('root'))
---
+++ src/seed.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import styles from  './seed.css'
+
+
+class Seed extends React.Component {
+    render () {
+        return <div className="yo">Grow seed of React, and harvest on this arable land~</div>
+    }
+}
+
+export default Seed
